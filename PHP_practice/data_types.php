0f6f95002b0c851dad1d078af7318a88ba0d3a6f
Dana Popa <popa.dana100@gmail.com>
2024-03-21 15:05:30 +0200

    data types

---
+++ PHP_practice/data_types.php
@@ -0,0 +1,18 @@
+<?php
+//$country = 'Romania';
+//$country = 'Romania';
+//echo 'Romania is a nice country';
+// $x = 20;
+// echo is_int($x);
+// $x = -45;
+// echo is_int($x);
+// $y = 30.56;
+// echo is_float($y);
+// echo PHP_FLOAT_MAX;
+// $a = false;
+// echo $a;
+// $a = 10;
+// $country = 'Romania';
+// $arr = [10,234,34,22,66];
+
+?>
