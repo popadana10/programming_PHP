4931e9e8ed00fd2d7d61b5d150012abdee90b1e0
Dana Popa <popa.dana100@gmail.com>
2024-03-21 15:23:59 +0200

    strings

---
+++ PHP_practice/strings.php
@@ -0,0 +1,64 @@
+<?php
+// Peter's car is very nice
+// $str = "Peter's car is very nice";
+// $str = 'Peter\'s car is very nice';
+// My country name is "Romania"
+//$str = "My country name is \"Romania\"";
+// $str = 'My country name is "Romania"';
+// echo $str;
+// function myCountry($city)
+// {
+//     echo 'Romania is a beautiful country and city is: '.$city;
+//     echo '<br>';
+// }
+// myCountry("Italy");
+// myCountry("France");
+// myCountry("Portugal");
+// $len = strlen("Romania is a nice country");
+// echo $len;
+//echo strtoupper("My name is Dana");
+//echo strtolower("My name is Dana");
+//echo ucwords("My name is Dana");
+// $str1 = trim("Romania ");
+// $str2 = trim(" USA");
+// echo $str1.$str2;
+
+// $str = "My country name is Romania. I love Romania. Romania is a beautiful country.";
+// echo str_replace("Romania", "USA", $str);
+
+//$date_of_birth = "2010-02-24";
+// 24/02/2010
+//0 1 2 3 4 5 6 7 8 9
+//2 0 1 0 - 0 2 - 2 4 
+// $year = substr($date_of_birth,0,4);
+// $month = substr($date_of_birth,5,2);
+// $day = substr($date_of_birth,8,2);
+//echo 'BD ' . ' is ' . ' a ' . ' good ' . ' country ';
+//echo 'BD' . ' ' . 'is' . ' ' . 'a' . ' ' . 'good'. ' ' . 'country';
+//echo $day . '/' . $month . '/' . $year;
+
+// $str = 'Romania';
+// echo chunk_split($str,3,'-');
+
+// $str = "Romania is a nice country";
+// echo count_chars($str,3);
+
+// $name = 'Morshedul Dana';
+// $age = 30;
+// echo sprintf("This user's name is: %s and his age is %d", $name, $age);
+
+// $str = 'Romania ';
+// echo str_repeat($str,20);
+
+// $str = "Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. Romania is a very nice country. ";
+// echo str_word_count($str);
+
+// $str = "Romania";
+// echo strrev($str);
+
+//echo strpos("Romania is my country", "country");
+
+// $str = '<script>alert(1)</script>';
+// echo strip_tags($str);
+
+?>
